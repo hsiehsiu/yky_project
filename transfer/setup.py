5b308107420b428c0c64ffa992bf62aeedcc05eb
hudenxiao <hudenxiao@gmail.com>
2025-11-06 16:18:09 +0800

    Update setup.py

--- transfer/setup.py
+++ transfer/setup.py
@@ -1,27 +1,30 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = 'transfer'
 
 setup(
     name=package_name,
-    version='0.1.0',
-    packages=[package_name],
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/transfer.launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='tmrdriver',
-    maintainer_email='tmrdriver@example.com',
-    description='camera→base tf + YOLO detections to JSON',
-    license='BSD',
-    tests_require=['pytest'],
+    maintainer='hsiu',
+    maintainer_email='billy940702@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
-            'trans = transfer.transfer:main',
+            'transfer = transfer.transfer:main',
         ],
     },
 )
